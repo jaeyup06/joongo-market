85dbb19b8486324ea846af3bed04487f9d249072
jaeyup06 <jaeyup06@gmail.com>
2024-10-06 06:47:33 +0900

    Page를 Suspense로 감싸기

--- src/app/(providers)/(root)/my/deals/page.tsx
+++ src/app/(providers)/(root)/my/deals/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Page from "@/app/components/Page";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 function MyDealsPage() {
@@ -22,35 +22,37 @@ function MyDealsPage() {
   };
 
   return (
-    <Page title={activeTab}>
-      {/* 전환하기 버튼 */}
-      <div className="space-x-4">
-        <button
-          onClick={() => handleTabChange("내 판매글")}
-          className={`${
-            activeTab === "내 판매글" ? "text-sky-500" : "text-black"
-          }`}
-        >
-          내 판매글
-        </button>
-        <button
-          onClick={() => handleTabChange("관심 판매글")}
-          className={`${
-            activeTab === "관심 판매글" ? "text-sky-500" : "text-black"
-          }`}
-        >
-          관심 판매글
-        </button>
-      </div>
-      <hr className="my-5" />
+    <Suspense>
+      <Page title={activeTab}>
+        {/* 전환하기 버튼 */}
+        <div className="space-x-4">
+          <button
+            onClick={() => handleTabChange("내 판매글")}
+            className={`${
+              activeTab === "내 판매글" ? "text-sky-500" : "text-black"
+            }`}
+          >
+            내 판매글
+          </button>
+          <button
+            onClick={() => handleTabChange("관심 판매글")}
+            className={`${
+              activeTab === "관심 판매글" ? "text-sky-500" : "text-black"
+            }`}
+          >
+            관심 판매글
+          </button>
+        </div>
+        <hr className="my-5" />
 
-      {/* 내용 */}
-      {activeTab === "내 판매글" ? (
-        <p>내 판매글 내용 표시</p>
-      ) : (
-        <p>관심 판매글 내용 표시</p>
-      )}
-    </Page>
+        {/* 내용 */}
+        {activeTab === "내 판매글" ? (
+          <p>내 판매글 내용 표시</p>
+        ) : (
+          <p>관심 판매글 내용 표시</p>
+        )}
+      </Page>
+    </Suspense>
   );
 }
 
