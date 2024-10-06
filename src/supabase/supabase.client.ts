78f15f7088e356e443debe696ec1d3ef9c510552
jaeyup06 <jaeyup06@gmail.com>
2024-10-07 01:53:15 +0900

    supabase URL 깃허브에 올라가도록 직접 담기

--- src/supabase/supabase.client.ts
+++ src/supabase/supabase.client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "./database.types";
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+const SUPABASE_URL = "https://ifebbbxnprxmzlzdcfua.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmZWJiYnhucHJ4bXpsemRjZnVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgwMjI5MjYsImV4cCI6MjA0MzU5ODkyNn0.IXPcy4TP_40FFB6nJrVb-CeIoNFQIvEZseMfM-z8m0k";
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
